a9f7d14dd518d4eb0a00be203f9e521611421f00
Ivan Kenevich <ivan.kenevich@daimler.com>
2023-05-10 19:01:40 -0700

    add teensy code to collect arm damping data add first dataset collected from arm

--- code/MATLAB/arm2_damping/main.m
+++ code/MATLAB/arm2_damping/main.m
@@ -1,6 +1,11 @@
 clear; close all; clc;
 
+%% actual behavior
+x = table2array(readtable("dataset1.txt"));
+figure
+plot(x(:,1), x(:,2))
 
+%% theoretical solution
 params.I = 10; % kg m^2
 params.b = 0.1; % N m s
 params.M = 1; % kg
@@ -10,4 +15,5 @@ params.g = 9.81; % m / s^2
 y0 = [-pi/2, 0];
 [t, y] = ode23(@(t,y) odefun(t, y, params), [0, 10], y0);
 
+figure
 plot(t, y(:,1))
---
+++ code/teensy/arm2_damping/arm2_damping.ino
@@ -0,0 +1,147 @@
+#include <Encoder.h>
+
+#define rad2deg (180.0 / M_PI)
+#define deg2rad (M_PI / 180.0)
+
+/*
+  Encoder 1 (motor-mounted) variables
+*/
+#define PIN_ENC1_A   2 // encoder pins for the motor
+#define PIN_ENC1_B   3
+Encoder enc1(PIN_ENC1_A, PIN_ENC1_B);
+volatile long enc1_pos_raw;               // counts read from the encoder
+volatile float enc1_pos, enc1_speed;      // position and speed of the encoder in physical units
+const size_t ENC1_POS_BUFF = 50;          // size of position buffer for velocity estimate
+float enc1_pos_buff[ENC1_POS_BUFF] = {0}; // position buffer for velocity estimate
+
+/*
+  Encoder 2 (free arm-mounted) variables
+*/
+#define PIN_ENC2_A   0 // encoder pins for the free arm
+#define PIN_ENC2_B   1
+Encoder enc2(PIN_ENC2_A, PIN_ENC2_B);
+volatile long enc2_pos_raw;               // counts read from the encoder
+volatile float enc2_pos, enc2_speed;      // position and speed of the encoder in physical units
+const size_t ENC2_POS_BUFF = 50;          // size of position buffer for velocity estimate
+float enc2_pos_buff[ENC2_POS_BUFF] = {0}; // position buffer for velocity estimate
+
+/*
+  Motor variables
+*/
+#define PIN_PWM1    29 // PWM pins for motor driver
+#define PIN_PWM2    30 
+volatile float pwm_out = 0, pwm_out_copy = 0;
+
+/*
+  Timing indicator pins
+*/
+#define PIN_TIMER_1   32 // timing indicator pin for isr_t1() function
+#define PIN_TIMER_2   31 // timing indicator pin for loop() function
+volatile char timer1_pin_state = 0, timer2_pin_state = 0;
+
+/*
+  Control switches
+*/
+#define PIN_SW_1    33 // motor enable pin
+volatile int motor_enabled = false;
+#define PIN_SW_2    39
+#define PIN_SW_3    15
+
+#define TIMER_CYCLE_MICROS 100
+const float Ts = 1e-6 * TIMER_CYCLE_MICROS;
+IntervalTimer t1;
+
+elapsedMicros t_us; // teensyduino variable type that increments by itself
+const float input_freq = 8, // Hz
+            input_amp  = 0, // V
+            supply_voltage = 12; // V
+float setpoint;
+            
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);
+
+  pinMode(PIN_TIMER_1, OUTPUT);
+  pinMode(PIN_TIMER_2, OUTPUT);
+  pinMode(PIN_SW_1, INPUT);
+  pinMode(PIN_SW_2, INPUT);
+  pinMode(PIN_SW_3, INPUT);
+
+  Serial.begin(57600);
+  
+  t1.begin(isr_t1, TIMER_CYCLE_MICROS);
+
+  digitalWrite(LED_BUILTIN, LOW);
+}
+
+void isr_t1(void) {
+  enc1_pos_raw = enc1.read(); // pulses
+  enc2_pos_raw = enc2.read();
+  enc1_pos = 360.0 * enc1_pos_raw / 1632.67; // degrees
+  enc2_pos = 360.0 * enc2_pos_raw / 8192.0;
+
+  // shift over position buffer and add the new measurement
+  for (int j = ENC1_POS_BUFF - 1; j > 0; j--) {
+    enc1_pos_buff[j] = enc1_pos_buff[j - 1];
+  }
+  enc1_pos_buff[0] = enc1_pos;
+
+  for (int j = ENC2_POS_BUFF - 1; j > 0; j--) {
+    enc2_pos_buff[j] = enc2_pos_buff[j - 1];
+  }
+  enc2_pos_buff[0] = enc2_pos;
+
+  // calculate speed as a secant line to position
+  enc1_speed = (enc1_pos_buff[0] - enc1_pos_buff[ENC1_POS_BUFF - 1]) / ((ENC1_POS_BUFF - 1) * Ts);
+  enc2_speed = (enc2_pos_buff[0] - enc2_pos_buff[ENC2_POS_BUFF - 1]) / ((ENC2_POS_BUFF - 1) * Ts);
+
+  setpoint = input_amp * sin(input_freq * 2 * M_PI * t_us * 1e-6);
+  pwm_out = 100.0 * setpoint / supply_voltage;
+
+  if (motor_enabled) {
+    write_pwm(pwm_out);
+  } else {
+    write_pwm(0);
+  }
+
+  digitalWrite(PIN_TIMER_1, timer1_pin_state ^= 1);
+}
+
+void write_pwm(float percent) {
+  if (percent > 0) {
+    analogWrite(PIN_PWM1, map(percent, 0, 100, 0, 256));
+    analogWrite(PIN_PWM2, 0);
+  }
+  else if (percent < 0) {
+    analogWrite(PIN_PWM1, 0);
+    analogWrite(PIN_PWM2, map(percent, -100, 0, 256, 0));
+  }
+  else {
+    analogWrite(PIN_PWM1, 0);
+    analogWrite(PIN_PWM2, 0);
+  }
+}
+
+void loop() {
+  //  noInterrupts();
+  //  pwm_out_copy = pwm_out;
+  //  interrupts();
+  //
+  //
+  Serial.print(t_us);
+  Serial.print("\t");
+//  Serial.print(enc2_pos);
+//  Serial.print("\t");
+  Serial.println(enc2_pos);
+
+
+  if (digitalRead(PIN_SW_1) == HIGH) {
+    motor_enabled = true;
+  } else {
+    motor_enabled = false;
+  }
+  
+  delay(20);
+  digitalWrite(PIN_TIMER_2, timer2_pin_state ^= 1);
+}
