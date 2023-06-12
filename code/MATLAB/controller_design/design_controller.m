f60d9ae4e2958f6913ce27736f0233edd0e63ecb
Ivan Kenevich <ivan.kenevich@daimler.com>
2023-06-12 16:51:57 -0700

    add digital controller design and begin printng C code

--- code/MATLAB/controller_design/design_controller.m
+++ code/MATLAB/controller_design/design_controller.m
@@ -4,6 +4,7 @@ load("../paper/derived_systems.mat")
 sys = systems.full_measured;
 
 %% design gain matrix
+Ts = 1e-3;
 tr = 0.5;
 PMO = 10;
 
@@ -21,6 +22,11 @@ K = place(sys.A, sys.B, p);
 
 sys_cl = sys;
 sys_cl.A = sys.A - sys.B * K;
+
+% digital controller design
+sysD = c2d(sys,Ts);
+pd = [exp(p(1)*Ts) exp(p(2)*Ts) exp(p(3)*Ts) exp(p(4)*Ts) exp(p(5)*Ts)];
+Kd = place(sysD.A,sysD.B,pd);
 
 %% design observer
 addpath("../utils")
@@ -43,28 +49,40 @@ xlabel('Time (s)')
 hold off
 
 %% simulate nonlinear system with a linear controller
-tspan = [0, 5];
-y0 = [0 deg2rad(165) 0 0 0];
-control_pars.K = K;
-[t, y] = ode45(@(t, y) sys_odefun(t, y, f_ddt1, f_ddt2, control_law(t, y, control_pars), motor_pars), tspan, y0);
-% torque = (y - ref) * -K';
+% tspan = [0, 5];
+% y0 = [0 deg2rad(165) 0 0 0];
+% control_pars.K = K;
+% [t, y] = ode45(@(t, y) sys_odefun(t, y, f_ddt1, f_ddt2, control_law(t, y, control_pars), motor_pars), tspan, y0);
+% % torque = (y - ref) * -K';
+% 
+% for row=1:height(y)
+%     yrow = y(row, :);
+%     
+%     V(row) = - K * (yrow - [yrow(1) pi 0 0 0])';
+%     if abs(V(row)) > motor_pars.Vmax
+%         V(row)= sign(V(row)) * 12;
+%     end
+% end
+% 
+% figure
+% subplot(2,1,1)
+% plot(t, y(:,1)*180/pi, t, y(:,2)*180/pi), title("Arm positions"), xlabel("Time [s]"), ylabel("Anle [deg]"), ylim([180 - 360, 180 + 360]), grid on
+% subplot(2,1,2)
+% plot(t, y(:,3)*180/pi/360, t, y(:,4)*180/pi/360), title("Arm velocities"), xlabel("Time [s]"), ylabel("Velocity [rps]"), grid on
 
-for row=1:height(y)
-    yrow = y(row, :);
-    
-    V(row) = - K * (yrow - [yrow(1) pi 0 0 0])';
-    if abs(V(row)) > motor_pars.Vmax
-        V(row)= sign(V(row)) * 12;
-    end
-end
-
-figure
-subplot(2,1,1)
-plot(t, y(:,1)*180/pi, t, y(:,2)*180/pi), title("Arm positions"), xlabel("Time [s]"), ylabel("Anle [deg]"), ylim([180 - 360, 180 + 360]), grid on
-subplot(2,1,2)
-plot(t, y(:,3)*180/pi/360, t, y(:,4)*180/pi/360), title("Arm velocities"), xlabel("Time [s]"), ylabel("Velocity [rps]"), grid on
+%% print C code
+A_1 = sysD.A - sysD.B * Kd - L * sysD.C; % A_1 from Turcic observer handout
+BD = sysD.B;
 
 
+sprintf('#define TIMER_CYCLE_MICROS %g',Ts*1e6)
+sprintf('a11 = %g, a12 = %g, a13 = %g,\na21 = %g, a22 = %g, a23 = %f,\na31 = %g, a32 = %g, a33 = %g;', AD(1,1), AD(1,2), AD(1,3), AD(2,1), AD(2,2), AD(2,3), AD(3,1), AD(3,2), AD(3,3))
+sprintf('b1 = %g, b2 = %g, b3 = %g;',BD(1), BD(2), BD(3))
+
+sprintf('Matrix<1,5> Kd = {%g, %g, %g, %g, %g};', Kd(1), Kd(2), Kd(3), Kd(4), Kd(5));
+
+
+%% functions
 function [out] = control_law(t, y, pars)
     out = - pars.K * (y - [y(1) pi 0 0 0]');
 end
