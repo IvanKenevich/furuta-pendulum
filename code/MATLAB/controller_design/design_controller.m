c9fff1b490b3a9396fedb6fafd5c7890fe2bcfc4
Ivan Kenevich <ik2@ds.cecs.pdx.edu>
2023-06-14 18:30:32 -0700

    design_controller.m - place nondominant closed loop poles farther away - zero out arm1 position gains
    build_ss_models.m
    - bring back export of function handles for nonlinear simulation
    
    derived_systems.mat
    - caused by changes to build_ss_models.m
    
    test_DigitalObserver.m
    - bring over Turcic's example code

--- code/MATLAB/controller_design/design_controller.m
+++ code/MATLAB/controller_design/design_controller.m
@@ -5,7 +5,7 @@ sys = systems.full_measured;
 
 %% design gain matrix
 Ts = 1e-3;
-tr = 0.5;
+tr = 0.3;
 PMO = 10;
 
 z=((-log(PMO/100))/(sqrt(log(PMO/100)^(2)+pi^(2))));
@@ -13,12 +13,13 @@ wn=((2.917*(z^(2)-0.142852245458*z+0.342817963661))/(tr));
 
 p1=-z*wn+wn*sqrt(1-z^2)*1i;
 p2=-z*wn-wn*sqrt(1-z^2)*1i;
-p3 = floor(5*real(p1));
+p3 = floor(20*real(p1));
 p4 = 1.1*p3;
 p5 = 1.1*p4;
 
 p = [p1 p2 p3 p4 p5];
 K = place(sys.A, sys.B, p);
+K(1) = 0;
 
 sys_cl = sys;
 sys_cl.A = sys.A - sys.B * K;
@@ -27,6 +28,10 @@ sys_cl.A = sys.A - sys.B * K;
 sysD = c2d(sys,Ts);
 pd = [exp(p(1)*Ts) exp(p(2)*Ts) exp(p(3)*Ts) exp(p(4)*Ts) exp(p(5)*Ts)];
 Kd = place(sysD.A,sysD.B,pd);
+Kd(1) = 0;
+
+sys_cl_D = sysD;
+sys_cl_D.A = sysD.A - sysD.B * Kd;
 
 %% design observer
 addpath("../utils")
@@ -49,26 +54,26 @@ xlabel('Time (s)')
 hold off
 
 %% simulate nonlinear system with a linear controller
-% tspan = [0, 5];
-% y0 = [0 deg2rad(165) 0 0 0];
-% control_pars.K = K;
-% [t, y] = ode45(@(t, y) sys_odefun(t, y, f_ddt1, f_ddt2, control_law(t, y, control_pars), motor_pars), tspan, y0);
-% % torque = (y - ref) * -K';
-% 
-% for row=1:height(y)
-%     yrow = y(row, :);
-%     
-%     V(row) = - K * (yrow - [yrow(1) pi 0 0 0])';
-%     if abs(V(row)) > motor_pars.Vmax
-%         V(row)= sign(V(row)) * 12;
-%     end
-% end
-% 
-% figure
-% subplot(2,1,1)
-% plot(t, y(:,1)*180/pi, t, y(:,2)*180/pi), title("Arm positions"), xlabel("Time [s]"), ylabel("Anle [deg]"), ylim([180 - 360, 180 + 360]), grid on
-% subplot(2,1,2)
-% plot(t, y(:,3)*180/pi/360, t, y(:,4)*180/pi/360), title("Arm velocities"), xlabel("Time [s]"), ylabel("Velocity [rps]"), grid on
+tspan = [0, 5];
+y0 = [0 deg2rad(165) 0 0 0];
+control_pars.K = K;
+[t, y] = ode45(@(t, y) sys_odefun(t, y, f_ddt1, f_ddt2, control_law(t, y, control_pars), motor_pars), tspan, y0);
+% torque = (y - ref) * -K';
+
+for row=1:height(y)
+    yrow = y(row, :);
+    
+    V(row) = - K * (yrow - [yrow(1) pi 0 0 0])';
+    if abs(V(row)) > motor_pars.Vmax
+        V(row)= sign(V(row)) * 12;
+    end
+end
+
+figure
+subplot(2,1,1)
+plot(t, y(:,1)*180/pi, t, y(:,2)*180/pi), title("Arm positions"), xlabel("Time [s]"), ylabel("Anle [deg]"), ylim([180 - 360, 180 + 360]), grid on
+subplot(2,1,2)
+plot(t, y(:,3)*180/pi/360, t, y(:,4)*180/pi/360), title("Arm velocities"), xlabel("Time [s]"), ylabel("Velocity [rps]"), grid on
 
 %% print C code
 A_1 = sysD.A - sysD.B * Kd - L * sysD.C; % A_1 from Turcic observer handout
@@ -105,4 +110,5 @@ fprintf(['Matrix<5,4> L = {%g, %g, %g, %g,\n' ...
 %% functions
 function [out] = control_law(t, y, pars)
     out = - pars.K * (y - [y(1) pi 0 0 0]');
+%     out = - pars.K * y;
 end
